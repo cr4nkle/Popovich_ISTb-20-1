e12d65af889a8c063bb5259acf0eb39af4515043
Сергей Попович <cr4nkle@yandex.ru>
2022-05-04 13:59:39 +0800

    Добавил поля и сделал контруктор

--- src/program/model/Cashier.java
+++ src/program/model/Cashier.java
@@ -1,4 +1,11 @@
 package program.model;
 
 public class Cashier {
+    private String fullName;
+    private String password;
+
+    public Cashier(String fullName, String password){
+        this.fullName = fullName;
+        this.password = password;
+    }
 }
--- src/program/model/Product.java
+++ src/program/model/Product.java
@@ -1,4 +1,15 @@
 package program.model;
 
 public class Product {
+    private int code;
+    private String name;
+    private int price;
+    private int quantity;
+
+    public Product(int code, String name, int price, int quantity){
+        this.code = code;
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
 }
